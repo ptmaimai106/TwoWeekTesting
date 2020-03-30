3d66eabb950a1981713c1fef6ebe8fa3010d1281
ptmaimai106 <ptmaimai106@gmail.com>
2020-03-30 14:49:20 +0700

    Implement game snake

--- Task2/script.js
+++ Task2/script.js
@@ -4,14 +4,39 @@ const row = document.getElementById('row');
 const col = document.getElementById('column');
 const buttonGenerate = document.querySelector('.generate');
 
+let values = [];
+
+window.onscroll = function() {myFunction()};
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
 
 function generateGrid(rows, cols) {
   grid.style.setProperty('--grid-rows', rows);
-  grid.style.setProperty('--grid-cols', cols);
+  grid.style.setProperty('--grid-cols', cols);  
+
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
+    let value = Math.floor(Math.random() *1000);
+    while(true){
+        if(values.length === 0){
+            values.push(value);
+            break;
+        }
+        if(values.indexOf(value) === -1){
+            values.push(value);
+            break;
+        }        
+        value = Math.floor(Math.random() *1000);
+    }
+    cell.innerText = (value);
     grid.appendChild(cell).className = "grid-item";
   };
 };
@@ -47,13 +72,3 @@ buttonGenerate.addEventListener('click', handleGenerate)
 
 
 
-window.onscroll = function() {myFunction()};
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
---
+++ Task3/script.js
@@ -0,0 +1,99 @@
+var KEY_LEFT = 37;
+var KEY_UP = 38;
+var KEY_RIGHT = 39;
+var KEY_DOWN = 40;
+
+
+var canvas, ctx;
+var gridSize = tileSize = 20; 
+var nextX = nextY = 0;
+var defaultSize =tailSize = 1;
+var snakeX = snakeY = 10;
+var foodX = foodY = 1;
+var snakeTrail = [];
+
+
+function render() {
+  snakeX += nextX;
+  snakeY += nextY;
+
+  if (snakeY < 0) {
+    snakeY = gridSize - 1;
+  }
+  if (snakeY > gridSize - 1) {
+    snakeY = 0;
+  }
+
+  if (snakeX < 0) {
+    snakeX = gridSize - 1;
+  }
+  if (snakeX > gridSize - 1) {
+    snakeX = 0;
+  }
+
+  if (snakeX == foodX && snakeY == foodY) {// eating
+
+    tailSize++;
+    foodX = Math.floor(Math.random() * gridSize);
+    foodY = Math.floor(Math.random() * gridSize);
+  }
+
+  ctx.fillStyle = "#ddd";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for(let i=0; i<20; i++){
+      for(let j=0; j<20; j++)
+        ctx.strokeRect(i*20, j*20, 20, 20);
+  }
+
+  ctx.fillStyle = "black";
+  for (var i = 0; i < snakeTrail.length; i++) {
+    ctx.fillRect(
+      snakeTrail[i].x * tileSize,
+      snakeTrail[i].y * tileSize,
+      tileSize,
+      tileSize
+    );
+
+    if (snakeTrail[i].x == snakeX && snakeTrail[i].y == snakeY) {
+      tailSize = defaultSize;
+    }
+  }
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(foodX * tileSize, foodY * tileSize, tileSize, tileSize);
+
+  snakeTrail.push({ x: snakeX, y: snakeY });
+  while (snakeTrail.length > tailSize) {
+    snakeTrail.shift();
+  }
+}
+
+function keyDownEvent(e) {
+  switch (e.keyCode) {
+    case KEY_LEFT:
+      nextX = -1;
+      nextY = 0;
+      break;
+    case KEY_UP:
+      nextX = 0;
+      nextY = -1;
+      break;
+    case KEY_RIGHT:
+      nextX = 1;
+      nextY = 0;
+      break;
+    case KEY_DOWN:
+      nextX = 0;
+      nextY = 1;
+      break;
+  }
+}
+
+window.onload = function() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    document.addEventListener("keydown", keyDownEvent);
+    setInterval(render, 1000/5 );
+};
+  
